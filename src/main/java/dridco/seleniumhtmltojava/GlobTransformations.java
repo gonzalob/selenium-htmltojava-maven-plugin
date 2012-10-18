454fa3ec44692e581ddad7d7372c6064d11c2fc1
Gonzalo Bermúdez <gonzalob@gonz0.com.ar>
2012-10-18 11:44:43 -0300

    Fixed the glob issue at last. Had some problems with my computer, so I'll run a couple more iterations of testing before actually releasing. Stay tuned.

--- src/main/java/dridco/seleniumhtmltojava/GlobTransformations.java
+++ src/main/java/dridco/seleniumhtmltojava/GlobTransformations.java
@@ -1,11 +1,9 @@
 package dridco.seleniumhtmltojava;
 
-import static java.lang.String.format;
-import static org.apache.commons.lang.StringUtils.EMPTY;
-
 public class GlobTransformations implements Transformations {
-
-	private String glob;
+	
+	private static final String ESCAPED_SLASH = "\\\\";
+	private final String glob;
 
 	public GlobTransformations(String pattern) {
 		this.glob = pattern;
@@ -16,22 +14,34 @@ public class GlobTransformations implements Transformations {
 	}
 
 	private String getPatternFromGlob() {
-		// http://stackoverflow.com/questions/1247772/is-there-an-equivalent-of-java-util-regex-for-glob-type-patterns#3619098
-		return "(\"^" + quoteStart() + "\" + \"" + globWithoutPrefix() + "\" + \"" + quoteEnd() + "$\")" + replaceStatement("\\\\*", ".*") + replaceStatement("\\\\?", ".");
-	}
-	
-	private String replaceStatement(String replaced, String replacement) {
-		return format(".replaceAll(\"" + replaced + "\", \"" + unquote(replacement) + "\")");
-	}
-	
-	private String unquote(String replacement) {
-		return quoteEnd() + replacement + quoteStart();
+		return "\"^" + quoteStart() + "\" + (\"" + globWithoutPrefix() + "\")" + replaceStatement(ESCAPED_SLASH + "*", ".*") + replaceStatement(ESCAPED_SLASH + "?", ".") + " + \"" + quoteEnd() + "$\"";
 	}
 
-	private String quoteStart() { return "\\\\Q"; }
-	private String quoteEnd()   { return "\\\\E"; }
+	private String replaceStatement(String replaced, String replacement) {
+		return ".replaceAll(\"" + replaced + "\", \"" + unquote(replacement) + "\")";
+	}
+
+	private String unquote(String replacement) {
+		return escapedEnd() + replacement + escapedStart();
+	}
+
+	private String escapedStart() {
+		return ESCAPED_SLASH + quoteStart();
+	}
+
+	private String escapedEnd() {
+		return ESCAPED_SLASH + quoteEnd();
+	}
+
+	private String quoteStart() {
+		return ESCAPED_SLASH + "Q";
+	}
+
+	private String quoteEnd() {
+		return ESCAPED_SLASH + "E";
+	}
 
 	private String globWithoutPrefix() {
-		return glob.replace("glob:", EMPTY);
+		return glob.replace("glob:", "");
 	}
 }
