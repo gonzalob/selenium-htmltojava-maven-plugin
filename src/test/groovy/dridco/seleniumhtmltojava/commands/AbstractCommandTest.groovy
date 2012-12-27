8feba611dc90503c0a39eef19106b18ec99e070c
Gonzalo Bermúdez <gonzalob@gonz0.com.ar>
2012-12-27 15:49:24 -0300

    Configured the compiler to store the dummy test classes inside maven's target directory, to keep the workspace clean

--- src/test/groovy/dridco/seleniumhtmltojava/commands/AbstractCommandTest.groovy
+++ src/test/groovy/dridco/seleniumhtmltojava/commands/AbstractCommandTest.groovy
@@ -10,27 +10,20 @@ import javax.tools.JavaCompiler.CompilationTask
 
 import junit.framework.Assert
 
-import org.apache.commons.logging.LogFactory
 import org.junit.Test
 
 abstract class AbstractCommandTest {
-
-	def logger
-
-	public AbstractCommandTest() {
-		logger = LogFactory.getLog(getClass())
-	}
 
 	@Test void canParseCommand() {
 		assertThat compileInstructions(), containsString(expectedResult())
 	}
 
 	@Test void canCompile() {
-		JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-		JavaFileObject file = new JavaSourceFromString("_SomeTestITCase", compileInstructions());
-		CompilationTask task = compiler.getTask(null, null, null, null, null, Arrays.asList(file));
+		JavaCompiler compiler = ToolProvider.getSystemJavaCompiler()
+		JavaFileObject file = new JavaSourceFromString("_SomeTestITCase", compileInstructions())
+		CompilationTask task = compiler.getTask(null, null, null, ["-d", "target"], null, Arrays.asList(file))
 
-		Assert.assertTrue(task.call());
+		Assert.assertTrue(task.call())
 	}
 
 	def compileInstructions() {
--- src/test/groovy/dridco/seleniumhtmltojava/commands/JavaSourceFromString.groovy
+++ src/test/groovy/dridco/seleniumhtmltojava/commands/JavaSourceFromString.groovy
@@ -6,15 +6,15 @@ import javax.tools.JavaFileObject.Kind
 // adapted from http://www.java2s.com/Code/Java/JDK-6/CompilingfromMemory.htm
 class JavaSourceFromString extends SimpleJavaFileObject {
 
-	final String code;
+	final String code
 
 	JavaSourceFromString(String name, String code) {
-		super(URI.create("string:///" + name + Kind.SOURCE.extension), Kind.SOURCE);
-		this.code = code;
+		super(URI.create("string:///" + name + Kind.SOURCE.extension), Kind.SOURCE)
+		this.code = code
 	}
 
 	@Override
 	public CharSequence getCharContent(boolean ignoreEncodingErrors) {
-		return code;
+		code
 	}
 }
